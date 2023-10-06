3a238f2da23a235a69965feca2344157a062e9d1
solowey <30394707+solowey1@users.noreply.github.com>
2023-10-06 06:04:24 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,59 @@
 const telegramBot = '@spacegametestbot';
+const message = [
+    {
+        name: 'check',
+        value: [
+            {
+                type: 'success', text: [
+                    'продолжить',
+                    'С возвращением!'
+                ]
+            }, {
+                type: 'error',
+                text: [`Игра запущена не через Telegram. Чтобы играть, запустите Telegram-бота ${telegramBot}`]
+            }
+        ]
+    }, {
+        name: 'signup',
+        value: [
+            {
+                type: 'success',
+                text: ['Вы успешно зарегистрировались.<br>Добро пожаловать в&nbsp;игру!']
+            }, {
+                type: 'error',
+                text: [
+                    'Произошла ошибка. Попробуйте зарегистрироваться снова через какое-то время',
+                    'Заполните обязательные поля',
+                    'Введите корректный email'
+                ]
+            }
+        ]
+    }
+];
+let telegramId;
+
+const setSize = () => {
+    const size = Math.round(window.innerWidth / 375 * 1000) / 1000;
+    document.body.style.setProperty('--size', size);
+}
+
+window.addEventListener('resize', (e) => {
+    setSize();
+})
+setSize();
+
+const getMessage = (name, type, n = 0) => {
+    const messageItem = message.find(item => item.name === name);    
+    if (messageItem) {
+        const valueItem = messageItem.value.find(item => item.type === type);
+        if (valueItem) {
+            return valueItem.text[n];
+        } else {
+            return 'Произошла неизвестная ошибка';
+        }
+    }
+    return 'Произошла неизвестная ошибка';
+}  
 
 // Header
 const header = document.getElementById('header');
@@ -8,6 +63,13 @@ const menu = document.getElementById('menu');
 const menuBtn = header.querySelector('.menu-icons');
 const menuBtnOpen = menuBtn.querySelector('.menu-open');
 const menuBtnClose = menuBtn.querySelector('.menu-close');
+const screenStart = document.getElementById('start');
+const screenSignup = document.getElementById('signup');
+const screenMap = document.getElementById('map');
+const startBtn = document.getElementById('start-button');
+const signupBtn = document.getElementById('signup-button');
+const levelBtns = document.querySelectorAll('.levels div');
+const inputTelegram = document.querySelector('[name="telegram_id"]');
 
 menuBtn.addEventListener('click', (e) => {
     if (menu.classList.contains('shown')) {
@@ -21,11 +83,53 @@ menuBtn.addEventListener('click', (e) => {
     }
 });
 
+const toast = (type, text = '') => {
+    const element = document.getElementById('toast');
+    element.classList.remove('shown', 'success', 'error');
+    element.innerHTML = text;
+    if (type !== '') element.classList.add(type);
+    setTimeout(() => {element.classList.add('shown')}, 300);
+    setTimeout(() => {element.classList.remove('shown')}, 2500);
+}
+
+signupBtn.addEventListener('click', (e) => {
+    e.target.classList.add('disabled');
+    const form = e.target.parentNode.querySelector('form');
+    const inputs = form.querySelectorAll('input');
+    inputs.forEach(input => {
+        input.classList.remove('error');
+        input.value === '' ? input.classList.add('error') : input.classList.remove('error');
+    });
+    const email = form.querySelector('[name="email"]').value;
+    const fullname = form.querySelector('[name="fullname"]').value;
+    const specialization = form.querySelector('[name="specialization"]').value;
+    const validateEmail = (email) => {
+        return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i.test(email);
+    }
+    if (email === '' || fullname === '' || specialization === '') {
+        toast('error', getMessage('signup', 'error', 1));
+        e.target.classList.remove('disabled');
+    } else if (!validateEmail(email)) {
+        inputs[0].classList.add('error');
+        toast('error', getMessage('signup', 'error', 2));
+        e.target.classList.remove('disabled');
+    } else {
+        const urlencoded = new URLSearchParams();
+        urlencoded.append('email', email.toLowerCase());
+        urlencoded.append('fullname', fullname);
+        urlencoded.append('specialization', specialization);
+        request('signup', urlencoded);
+    }
+
+});
+
 // request
-const request = (urlencoded) => {
+const request = (method, urlencoded) => {
     const url = 'https://n8n.solowey.ru/webhook/2639b53b-c7fa-493b-9b69-f74aad2dfb9e';
     const myHeaders = new Headers();
     myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+    urlencoded.append('method', method);
+    urlencoded.append('telegram_id', telegramId);
     var requestOptions = {
         method: 'POST',
         headers: myHeaders,
@@ -35,30 +139,81 @@ const request = (urlencoded) => {
     fetch(url, requestOptions)
     .then(response => response.json())
     .then(result => {
-        console.log(result)
-        document.getElementById('debug-id').innerHTML = result.telegram_id;
-        if (result.hasOwnProperty('code') && result.code === 0) {
-            if (result.message === 'No item to return got found') {
-                // отправить пользователю экран регистрации
+        if (method === 'check') {
+            if (result.hasOwnProperty('code') && result.code === 0) {
+                startBtn.setAttribute('href', '#signup');
+            } else if (result.hasOwnProperty('code') && result.code === 1) {
+                document.getElementById('debug-id').innerHTML = telegramId;
+                toast('', getMessage('check', 'success', 1));
+                startBtn.innerHTML = getMessage('check', 'success');
             }
-        } else {
-            // отправить пользователю карту выбора уровней
+            startBtn.classList.add('shown');
+        } else if (method === 'signup') {
+            toast('success', getMessage('signup', 'success', 0));
+            setTimeout(() => {
+                document.getElementById('signup').classList.add('hide');
+                // document.getElementById('signup').remove();
+                document.getElementById('signup-button').classList.remove('disabled');
+                setMap();
+            }, 1000);
+        } else if (method === 'map') {
+            // const levels = screenMap.querySelector('.levels div');
+            // levels.forEach(level => {
+            //     // result.
+            // });
         }
     })
-    .catch(error => console.log('error', error));
+    .catch(error => {
+        console.log('error', error);
+        if (method === 'signup') {
+            document.getElementById('signup-button').classList.remove('disabled');
+            toast('error', getMessage('signup', 'error', 0));
+        }
+    });
 }
 
-// user_id
-let userId;
-const checkInBase = (id) => {
-    const params = new URLSearchParams(id);
-    if (params.get('user_id') !== null) {
-        userId = params.get('user_id');
+// Find user by telegram user_id
+const checkInBase = (urlparams) => {
+    const params = new URLSearchParams(urlparams);
+    if (params.get('telegram_id') !== null) {
+        telegramId = parseInt(params.get('telegram_id'), 10);
         const urlencoded = new URLSearchParams();
-        urlencoded.append('id', userId);
-        request(urlencoded);
+        request('check', urlencoded);
     } else {
-        alert(`Игра запущена не через Telegram. Чтобы играть, запустите Telegram-бота ${telegramBot}`);
+        alert(getMessage('check', 'error', 0));
     }
 }
 checkInBase(window.location.search);
+
+// Get scores and set map
+const setMap = () => {
+    Math.ceil(window.innerWidth * 7.114);
+    const urlencoded = new URLSearchParams();
+    request('map', urlencoded);
+    document.dispatchEvent(new Event('map'));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // setMap();
+});
+
+// Button listeners
+startBtn.addEventListener('click', (e) => {
+    if (e.target.hash === '#signup') {
+        screenStart.classList.add('hide');
+        screenSignup.classList.remove('hide');
+    } else if (e.target.hash === '#start') {
+        setMap();
+        screenStart.classList.add('hide');
+        screenMap.classList.remove('hide');
+    }
+});
+
+levelBtns.forEach(levelBtn => {
+    levelBtn.addEventListener('click', (e) => {
+        const levelInfo = document.getElementById('level-info');
+        const levelNumber = levelInfo.querySelector('.level-number');
+        levelNumber.innerHTML = e.target.innerHTML;
+        levelInfo.classList.remove('hide');
+    });
+});
