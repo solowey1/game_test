70d37993d5bfcbfb709c04a389029ef5bdb60b67
solowey <30394707+solowey1@users.noreply.github.com>
2023-10-06 21:44:18 +0300

    Update map.js

--- assets/map.js
+++ assets/map.js
@@ -1,22 +1,48 @@
-const scrollboosterOptions = {
-    viewport: screenMap,
+// const scrollboosterOptions = {
+//     viewport: screenMap,
+//     content: screenMap.querySelector('.map'),
+//     scrollMode: 'transform',
+//     direction: 'vertical',
+//     bounce: false,
+//     friction: 0.3,
+//     emulateScroll: true,
+//     lockScrollOnDragDirection: 'vertical',
+//     dragDirectionTolerance: 40
+// }
+// const sb = new ScrollBooster(scrollboosterOptions);
+
+const lenis = new Lenis({
+    wrapper: screenMap,
     content: screenMap.querySelector('.map'),
-    scrollMode: 'transform',
-    direction: 'vertical',
-    bounce: false,
-    friction: 0.3,
-    emulateScroll: true,
-    lockScrollOnDragDirection: 'vertical',
-    dragDirectionTolerance: 40
+    // eventsTarget: screenMap.querySelector('.map-level-number'),
+});
+lenis.on('scroll', (e) => {
+    // console.log(e);
+})
+const raf = (time) => {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
 }
-const sb = new ScrollBooster(scrollboosterOptions);
+requestAnimationFrame(raf);
 
 document.addEventListener('map', (e) => {
+    screenStart.classList.add('hide');
+    screenSignup.classList.add('hide');
+    screenMap.classList.remove('hide');
+    const mapBottom = screenMap.scrollHeight - window.innerHeight;
+    const levels = document.querySelectorAll('.map-level-number');
+    const levelsComplete = Array.from(levels).filter(level => level.classList.contains('complete'));
+    const lastLevelComplete = levelsComplete.length ? levelsComplete[levelsComplete.length - 1] : levels[0];
+    const lastLevelCompleteTop = Math.ceil(lastLevelComplete.getBoundingClientRect().top);
+    const coordY = Math.min(lastLevelCompleteTop - window.innerHeight, mapBottom);
     setTimeout(() => {
-        const mapBottom = screenMap.scrollHeight - window.innerHeight;
-        const level1 = document.getElementById('level-1');
-        const level1Top = level1.getBoundingClientRect().top;
-        const coordY = Math.min(level1Top - 100, mapBottom);
-        sb.scrollTo({ x: 0, y: coordY });
+        // sb.scrollTo({ x: 0, y: coordY });
+        lenis.scrollTo(coordY, {
+            duration: 2,
+            easing: (x) => {
+                return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+            },
+            lock: true
+        });
     }, 1000);
 });
