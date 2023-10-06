c9ece93fc49e636a8083c052b58724886a38c33a
solowey <30394707+solowey1@users.noreply.github.com>
2023-10-06 16:34:32 +0300

    Update map.js

--- assets/map.js
+++ assets/map.js
@@ -1,29 +1,18 @@
-const lenis = new Lenis({
-    wrapper: screenMap,
+const sb = new ScrollBooster({
+    viewport: screenMap,
     content: screenMap.querySelector('.map'),
-    smoothTouch: true,
-
-})
-
-lenis.on('scroll', (e) => {
-//   console.log(e)
-})
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
+    scrollMode: 'transform',
+    direction: 'vertical',
+    bounce: false,
+    friction: 0.3
+});
 
 document.addEventListener('map', (e) => {
     setTimeout(() => {
         const mapBottom = screenMap.scrollHeight - window.innerHeight;
         const level1 = document.getElementById('level-1');
         const level1Top = level1.getBoundingClientRect().top;
-        const coordY = Math.min(level1Top, mapBottom);
-        lenis.scrollTo(coordY, {
-            offset: 100
-        })
+        const coordY = Math.min(level1Top - 100, mapBottom);
+        sb.scrollTo({ x: 0, y: coordY });
     }, 1000);
 });
